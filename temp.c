f059dcfad89303572b3790910ba4f53e16a5ae7b
ohmschrodinger <omdhamame@gmail.com>
2024-10-15 19:40:05 +0530

    Attribute and Nth child

---
+++ temp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+void printAdjMatrix(int arr[][v]){
+    int i , j;
+    for (i=0;i<v;i++){
+        printf("%d;", i);
+        for(j=0;j<v;j++){
+            printf("%d",arr[i][j];)
+            
+        }
+        printf("\n");
+    }
+}
