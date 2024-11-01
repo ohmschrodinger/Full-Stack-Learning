7d2f1b8db8d8bb16fcece9f7352fd1cc1af676c4
ohmschrodinger <omdhamame@gmail.com>
2024-11-01 13:32:22 +0530

    .

---
+++ 04. Mongo DB/01_setup.js
@@ -0,0 +1,11 @@
+// mongosh
+// show dbs
+// use database_name
+// CRUD OPERATIONS
+// 1. CREATE
+// 2. READ
+// 3. UPDATE
+// 4. DELETE
+
+
+"https://www.mongodb.com/docs/manual/tutorial/query-documents/"
---
+++ 04. Mongo DB/03_read.js
@@ -0,0 +1,7 @@
+db.inventory.find( {} )
+// returns all the documents / fetch all
+
+db.inventory.find({qty: 25})
+// returns all the documents whose  qty is 25 / fetch all matching documents
+
+
