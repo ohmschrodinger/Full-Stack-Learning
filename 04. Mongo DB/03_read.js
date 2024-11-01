b121d208239960808ee771a1cb6223dca21330c7
ohmschrodinger <omdhamame@gmail.com>
2024-11-01 22:16:27 +0530

    mongo db

--- 04. Mongo DB/03_read.js
+++ 04. Mongo DB/03_read.js
@@ -4,4 +4,23 @@ db.inventory.find( {} )
 db.inventory.find({qty: 25})
 // returns all the documents whose  qty is 25 / fetch all matching documents
 
+db.inventory.find( { status: "D" } )
+db.inventory.find( { status: { $in: [ "A", "D" ] } } )
+db.inventory.find( { status: "A", qty: { $lt: 30 } } )
+
+// or operator
+
+db.inventory.find( { $or: [ { status: "A" }, { qty: { $lt: 30 } } ] } )
+
+
+
+
+
+
+
+// update one, update many, replace
+
+// check out piplelining from mongo db  documentation
+// https://docs.mongodb.com/manual/core/pipeline-operations/
+
 
