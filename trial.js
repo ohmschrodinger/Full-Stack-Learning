3d6308a8d1b512474d2b3bb4797b8e5be919f483
ohmschrodinger <omdhamame@gmail.com>
2024-11-04 14:05:08 +0530

    "Added HTML elements (h1 and button) and JavaScript code to toggle text content on button click"

--- trial.js
+++ trial.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+let text = document.getElementById("temp");
+// let btn = document.getElementById("btn");
+
+// let num = 0;
+// btn.onclick = () =>
+// {
+//     if(num===0)
+//     {
+//         num=1;
+//         text.innerText = "i am learning react";
+//     }
+    
+//     if(num===1)
+//         {
+            
+//             num=0;
+//             text.innerText = "i am learning javascript";
+//     }
+// }
+function ff(){
+if(text.textContent == "I like javascript"){
+    text.textContent = "React"
+} else {
+    text.textContent = "I like javascript" 
+}
+}
+
