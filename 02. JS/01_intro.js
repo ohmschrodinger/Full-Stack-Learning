8421bde629ef58dc83143cad8ffd2d730a80bf17
ohmschrodinger <omdhamame@gmail.com>
2025-01-18 03:01:29 +0530

    js variables

---
+++ 02. JS/01_intro.js
@@ -0,0 +1,25 @@
+let age = 18;
+console.log(age);
+age = 'om';
+
+const pi = 3.14;
+
+console.log(age);
+console.log(typeof age);
+
+
+isFollow = true;
+
+
+// student is object
+const student = {
+    // key : 'value' 
+    name : 'Om' ,
+    age : 18,
+    class: 12
+};
+
+console.log(student);
+console.log(student.age);
+console.log(student['age']);
+
