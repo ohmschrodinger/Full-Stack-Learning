3bf2dec663e58f0060e1c74cf2b54695a128ea35
ohmschrodinger <omdhamame@gmail.com>
2025-01-18 20:40:14 +0530

    operators

---
+++ 02. JS/02_Operators.js
@@ -0,0 +1,38 @@
+let a;
+let x = 10, y = 20;
+console.log(a); 
+a = x % y;
+console.log(a); 
+a = x+y;
+console.log(a);
+a = x - y;
+console.log(a);
+a = x + y;
+console.log(a);
+a = x**y;
+console.log(a);
+a = x * y;
+console.log(a);
+a = x / y;
+console.log(a);
+
+let b;
+
+a = 10;
+b = "10";
+
+console.log(a==b);
+console.log(a!=b);
+console.log(a===b);
+
+
+let age = 30;
+if(age>=18)
+    {
+        console.log("Can vote.");
+    }
+    
+else{
+        console.log("cant vote.");
+    }
+    
---
+++ 02. JS/practice.js
@@ -0,0 +1,16 @@
+let a;
+let b;
+
+a = 10;
+b = "10";
+console.log(a===b);
+
+let age = 30;
+if(age>=18)
+{
+    console.log("Can vote.");
+}
+
+else{
+    console.log("cant vote.");
+}
